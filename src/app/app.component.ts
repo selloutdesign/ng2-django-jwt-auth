71f3f3eec4f0c9c3c1b1b801f2296d89a6f6ccff
Josh <j.selloutdesign@gmail.com>
2017-08-21 16:53:23 -0700

    authHttp attempt

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,21 @@
 import { Component } from '@angular/core';
 
 import { Http, RequestOptions, Headers, Response } from '@angular/http';
-import { AuthHttp, AuthConfig } from 'angular2-jwt';
+// import { AuthHttp, AuthConfig } from 'angular2-jwt';
+
+import { JobsService } from './jobs.service';
+
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/toPromise';
+
+import { Observable }        from 'rxjs/Observable';
+// Observable class extensions
+import 'rxjs/add/observable/of';
+
+// Observable operators
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/operator/debounceTime';
+import 'rxjs/add/operator/distinctUntilChanged';
 
 @Component({
   selector: 'app-root',
@@ -12,13 +24,15 @@ import 'rxjs/add/operator/toPromise';
 })
 
 export class AppComponent {
+  jobs: Observable<any>;
   thing: any;
   title = 'app works!';
   body = 'username=jsells&password=H0n0rFa1th1985&client_id=VisKOOvUVtPHCkLryKieu9Nse86EpkuqUNRMrbpm&grant_type=password';
   // private headers = new Headers({'Content-Type': 'application/x-www-form-urlencoded'});
 
   constructor(
-    private authHttp: AuthHttp,
+    // private authHttp: AuthHttp,
+    private js: JobsService,
     private http: Http,
   ) { }
   
@@ -27,18 +41,35 @@ export class AppComponent {
     console.log('Changed');
   }
 
+  getJobs() {
+    let headers = new Headers();
+    // headers.append('Authorization', "JWT " + localStorage.getItem('token'));
+    let options = new RequestOptions({headers: headers, method: 'get'});
+
+    this.js.getJobs('https://work-4-la-poundimal.c9users.io/auth/jobs/', options)
+      .subscribe(
+        data => {
+          let res = data.json();
+          this.jobs = res;
+          console.log(this.jobs);
+        },
+        err => console.log(err),
+        () => console.log('Request Complete')
+      );
+  }
+
   getGroups() {
     let b = 'username=jsells&password=H0n0rFa1th1985&client_id=VisKOOvUVtPHCkLryKieu9Nse86EpkuqUNRMrbpm&grant_type=password';
     let headers = new Headers();
     headers.append('Authorization', 'JWT ' + localStorage.getItem('token'));
     let options = new RequestOptions({ headers: headers, method: 'get' });
     
-    this.http.get('http://127.0.0.1:8000/groups/', options)
+    this.http.get('https://work-4-la-poundimal.c9users.io/groups/', options)
       .subscribe(
         data => {
           let res = data.json();
           console.log(res);
-          this.thing = data; console.log(data);
+          this.thing = data;
         },
         err => console.log(err),
         () => console.log('Request Complete')
@@ -51,7 +82,7 @@ export class AppComponent {
     let headers = new Headers({ 'Content-Type': 'application/x-www-form-urlencoded' });
     headers.append('x-requested-with','XMLHttpRequest');
     let options = new RequestOptions({ headers: headers, withCredentials: true });
-    this.http.post('http://127.0.0.1:8000/api-token-auth/', b, options)
+    this.http.post('https://work-4-la-poundimal.c9users.io/api-token-auth/', b, options)
       .map(res => {
         console.log(res.json());
         this.thing = res.json();
--- src/app/jobs.service.ts
+++ src/app/jobs.service.ts
@@ -1,8 +1,20 @@
 import { Injectable } from '@angular/core';
+import { AuthHttp, AuthConfig } from 'angular2-jwt';
+
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
 
 @Injectable()
 export class JobsService {
 
-  constructor() { }
+  constructor(
+    private authHttp: AuthHttp,
+  ) {}
+
+  getJobs(): Observable<any> {
+    return this.authHttp
+      .get('https://work-4-la-poundimal.c9users.io/auth/jobs/')
+      .map(res => res.json());
+  }
 
 }
